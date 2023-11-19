5c05aba3a5c2aaed3a154a667549d23e23932400
Home User <riddlej9879@yahoo.com>
2023-11-18 18:01:23 -0600

    My program broke so I added the database to C# programmatically instead of with SSMS. It may work perferctly after a merge and pull.

--- CS488LocalDB/Form1.Designer.cs
+++ CS488LocalDB/Form1.Designer.cs
@@ -29,13 +29,59 @@ namespace CS488LocalDB
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menu Choices";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(437, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(351, 426);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Order";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Place Order";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CS488LocalDB/Form1.cs
+++ CS488LocalDB/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,37 @@ namespace CS488LocalDB
         public Form1()
         {
             InitializeComponent();
+
+            Env ConnString = new Env();
+            string ServerConn = ConnString.SrvConn;
+            SqlConnection conn = new SqlConnection(connectionString: ServerConn);
+
+            try
+            {
+                SqlCommand get_menu = new SqlCommand("Select * from menu_items", conn);
+                conn.Open();
+                SqlDataReader menu = get_menu.ExecuteReader();
+
+                while (menu.Read())
+                {
+                    if (!(menu.Equals(null)))
+                    {
+                        for (int i = 0; i < menu.FieldCount; i++)
+                        {
+                            string myString = menu.GetValue(i).ToString();
+                            Console.WriteLine(myString);
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlException)
+            {
+                MessageBox.Show(sqlException.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
